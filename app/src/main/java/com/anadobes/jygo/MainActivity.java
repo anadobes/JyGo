2300092a54448a800c0a46bb71762e1ce2c1bdbe
Gurvan HENRY <gurvan.henry@scriptandgo.com>
2015-05-21 13:42:01 +0200

    Initial commit

---
+++ app/src/main/java/com/anadobes/jygo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.anadobes.jygo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendSMSMessage("0630912717", "J'y Go"); // Me
+        //sendSMSMessage("0625957656", "J'y Go"); // Mat
+        finish();
+    }
+
+    protected void sendSMSMessage(String phoneNo, String message) {
+        Log.i("Send SMS", "");
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+}
